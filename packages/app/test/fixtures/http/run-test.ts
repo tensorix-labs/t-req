4f81185429e9ed21fdee512c22b4cecf99ae394b
Andrew Melchor <andrew.melchor@gmail.com>
2026-01-26 11:16:38 -0800

    update readme

--- packages/app/test/fixtures/http/run-test.ts
+++ packages/app/test/fixtures/http/run-test.ts
@@ -1,14 +1,13 @@
 import { createClient } from '@t-req/core';
 
 const client = createClient({
-  server: process.env.TREQ_SERVER ?? 'http://localhost:4096',
   variables: {
     baseUrl: 'https://httpbin.org/',
     userId: '123',
     token: '1234567890'
   }
 });
-console.log({ client });
+
 console.log('Running simple GET request...');
 await client.run('./post-json.http');
 
